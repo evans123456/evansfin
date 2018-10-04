f483bd857071772fd016da3753314f6b4e030995
evans123456 <evanpersie3@gmail.com>
2018-10-04 21:45:48 +0300

    fix the jquery responsible for scrolling

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,7 +1,7 @@
 import React , { Component } from 'react'
-import {Grid , Cell} from 'react-mdl'
+//import {Grid , Cell} from 'react-mdl'
 import './../App.css'
-import {Link} from 'react-router-dom';
+//import {Link} from 'react-router-dom';
 
 class Landing extends Component{
 
--- src/components/Photography.js
+++ src/components/Photography.js
@@ -1,11 +1,45 @@
 import React, { Component } from 'react'
+import './Photography.css'
+import $ from 'jquery'
 
-export default class Photography extends Component {
+ class Photography extends Component {
+
+  componentWillMount(){
+    $(window).on('scroll',function(){
+      console.log('inside jquery')
+    })
+    console.log('OUTSIDE JQUERY')
+  }
+
+  handleResize(){
+    $(window).scroll(function(){
+      console.log('inside jquery')
+    })
+    console.log('OUTSIDE JQUERY')
+
+  }
+  
+
   render() {
     return (
-      <div>
+      <div className='Container' >
+      {this.handleResize()}
+
+
+
+          <header className='bird-box'>
+
+                <div className = 'fore-bird' >
+                 
+                </div>
+
+          </header> 
+
+
         <h1>photography </h1>
       </div>
     )
   }
 }
+
+export default Photography;
